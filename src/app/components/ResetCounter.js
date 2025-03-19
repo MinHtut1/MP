7ad339ad3f47f10a4118509c351c5857cb6abb8b
Min Htut <gmin69620@gmail.com>
2025-03-19 15:21:50 +0700

    addrsetC.js

---
+++ src/app/components/ResetCounter.js
@@ -0,0 +1,47 @@
+'use client';
+import { useState } from 'react';
+import './ResetCounter.css';
+//import Counter from "@/app/components/Counter";
+
+export default function ResetCounter() {
+    const [showB, setShowB] = useState(true);
+    return (
+        <div>
+            {showB && <Counter />}
+            <Counter />
+            <label>
+                <input
+                    type="checkbox"
+                    checked={showB}
+                    onChange={e => {
+                        setShowB(e.target.checked)
+                    }}
+                    />
+                Render the second counter
+            </label>
+        </div>
+    );
+}
+
+function Counter() {
+    const [score, setScore] = useState(
+        const [hover, setHover] = useState(false);
+
+    let className = 'counter';
+    if (hover) {
+        className += ' hover';
+    }
+
+    return (
+        <div
+            className={className}
+            onPointerEnter={() => setHover(true)}
+            onPointerLeave={() => setHover(false)}
+        >
+            <h1>{score}</h1>
+            <button onClick={() => setScore(score + 1)}>
+                Add one
+            </button>
+        </div>
+    );
+}
