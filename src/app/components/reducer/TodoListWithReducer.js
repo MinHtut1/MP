ab07023f7b5033a9e523968a37e926766911182a
gmin69620@gmail.com <gmin69620@gmail.com>
2025-03-28 20:47:51 +0700

    add

--- src/app/components/reducer/TodoListWithReducer.js
+++ src/app/components/reducer/TodoListWithReducer.js
@@ -28,6 +28,8 @@ function todoReducer(state,action)
             return [...state, action.payload];
         case 'DELETE_TODO':
             return state.filter(todo=>todo.id != action.payload.id);
+        case 'UPDATE_TODO':
+            return state.map(todo => todo.id === action.payload.id ? action.payload : todo);
     }
     return state;
 }
@@ -65,12 +67,13 @@ function createAction(type,payload)
     }
 }
 
-function TodoInput({dispatch,editTodo}) {
+function TodoInput({dispatch,editTodo,clearEdit}) {
         
     console.log('Todo input render',editTodo);
     const title = editTodo!==null?editTodo.title:'';
     console.log("Title", title);
     
+    //keep track to update state whenever prop change
     useEffect( ()=>{
         if(editTodo)
         {
@@ -85,20 +88,29 @@ function TodoInput({dispatch,editTodo}) {
          if (editTodo)
                 {    
                     //Update case
+                    console.log('Update case');
+                    const updateTodo = {
+                        id : editTodo.id,
+                        title : todoText
+                    };
+                    let updateAction = createAction('UPDATE_TODO',updateTodo);
+                    dispatch(updateAction);
+                    setTodoText('');
+                    clearEdit();
                 }
                 else
                 {
                    let newId = nextId();
-                    const newTodo = {
+                    const newToDo = {
                         id : newId,
-                        title : todoText
+                        title : todoText    
                     };
-                    let addTodoAction = createAction('ADD_TODO',newTodo);
+                    let addTodoAction = createAction('ADD_TODO', newToDo);
                     dispatch(addTodoAction);
                     setTodoText(''); 
                 }
-        
     };
+
     return <div>
         <form>
             <div className={"form-group"}>
@@ -107,7 +119,8 @@ function TodoInput({dispatch,editTodo}) {
                         onChange={(event)=> setTodoText(event.target.value)}
                        className={"form-control-sm"}/>
                 <button type={"button"}
-                        className={"btn btn-primary"} 
+
+className={"btn btn-primary"} 
                         onClick={addHandler}
                  >Add
                  </button>
@@ -130,14 +143,19 @@ export default function TodoListWithReducer()
         console.log('set edit to do',todo);
         setEditToDo(todo);
     }
+    const clearEdit = () => {
+        setEditToDo(null);
+    }
     return (<div>
-        <TodoInput dispatch={dispatch} editTodo={editTodo}/>
+        <TodoInput dispatch={dispatch} 
+                    editTodo={...editTodo}  
+                    clearEdit ={clearEdit}/>
         <div>
             {
                 todos.map(todo => <TodoItem key={todo.id}
-                                         todo={todo}
-                                         editHandler = {editHandler}    
-                                         dispatch={dispatch}/>)
+                                            todo={todo}
+                                            editHandler = {editHandler}    
+                                            dispatch={dispatch}/>)
             }
         </div>
     </div>)
